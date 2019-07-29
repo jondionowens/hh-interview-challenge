0af0ceec8bae7b4f575ccea7edc2bc38023e3536
Jonathan Owens <jowens0184@gmail.com>
2019-07-28 20:17:46 -0700

    Fixed async bug

--- server.js
+++ server.js
@@ -12,22 +12,27 @@ app.get('/swatches/', (req, res) => {
 
 app.get('/swatches/:family', (req, res) => {
   if (req.params.family === 'reds' || req.params.family === 'red') {
-    let parsedData;
     fs.readFile('./data.json', (err, data) => {
       if (err) { throw err };
-      parsedData = JSON.parse(data);
+      let parsedData = JSON.parse(data);
       res.json(parsedData);
     })
+  } else if (req.params.family === 'greens' || req.params.family === 'green') {
+    fs.readFile('./data.json', (err, data) => {
+      if (err) { throw err };
+      let parsedData = JSON.parse(data);
+      res.json(parsedData);
+    })
+  } else {
+    fs.readFile('./data.json', (err, data) => {
+      if (err) { throw err };
+      let parsedData = JSON.parse(data);
+      res.json(parsedData);
+    })
+  }
 
-    
-  } 
-
-  if (req.params.family === 'greens' || req.params.family === 'green') {
-    res.json({ "get": "greens" });
-  } 
-
-  // Catch-all, returns all swatches if there isn't a matching family
-  res.json({ "get": "all" });
+  /// Catch-all, returns all swatches if there isn't a matching family
+ 
 
 });
 
