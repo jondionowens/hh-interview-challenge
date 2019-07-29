2680f00df0953b5d5ebeee835e7b73a8bc79007d
Jonathan Owens <jowens0184@gmail.com>
2019-07-28 19:28:36 -0700

    Reading data from file

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const data = 
+const fs = require('fs');
 
 app.use('/', express.static(path.join(__dirname, './public')));
 
 app.get('/swatches/', (req, res) => {
-    res.json({"get": "all"});
+  res.json({ "get": "all" });
 });
 
 app.get('/swatches/:family', (req, res) => {
-  if (req.params.family === 'reds') {
-    res.json({"get": "red"});
-  }
+  if (req.params.family === 'reds' || req.params.family === 'red') {
+    let parsedData;
+    fs.readFile('./data.json', (err, data) => {
+      if (err) { throw err };
+      parsedData = JSON.parse(data);
+      res.json(parsedData);
+    })
+
+    
+  } 
+
+  if (req.params.family === 'greens' || req.params.family === 'green') {
+    res.json({ "get": "greens" });
+  } 
+
+  // Catch-all, returns all swatches if there isn't a matching family
+  res.json({ "get": "all" });
+
 });
 
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}!`);
-})
+app.listen(port, () => { console.log(`App running on port ${port}!`) });
