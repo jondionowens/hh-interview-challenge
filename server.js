fd6037af788d120c16b0fe8253fd31995fc79297
Jonathan Owens <jonathano@metia.com>
2019-07-30 12:46:48 -0700

    Updated port to look for heroku env var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
